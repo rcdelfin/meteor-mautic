b378568e6bb35a2442f7f94ed83da4035408fad3
Raymund Delfin <raymund@ultro.co.uk>
2016-05-12 02:33:15 +0800

    bump version

--- package.js
+++ package.js
@@ -11,7 +11,6 @@ Package.onUse(function(api) {
   api.use('service-configuration', ['client', 'server']);
   api.use('oauth2', ['client', 'server']);
   api.use('oauth', ['client', 'server']);
-  api.use('momentjs:moment', ['client', 'server']);
   api.use('random', ['client', 'server']);
   api.use('http', 'server');
   api.use('underscore', 'server');
